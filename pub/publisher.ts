67cf39b9ffa5b9471daa3c2068466326d1073470
Oscar Rosello <oskietimg@gmail.com>
2024-01-30 01:35:22 +0100

    Cambios en el pub sub para adaptarse a las dos colas (Pendiente de fusionarse con worker i front). Instalacion de docker e inicio de api en proceso en el worker

--- pub/publisher.ts
+++ pub/publisher.ts
@@ -4,9 +4,7 @@ async function pubmessage(){
     try {
         let nc = await connect({ servers: ['nats://nats:4222', 'nats://nats-1:4222', 'nats://nats-2:4222']});
     
-        nc.publish('Work', 'Do something!');
-        console.log("Publicado")
-
+        nc.publish("WorkerQueue", "Do something!");
         await nc.flush();
         await nc.close();
     } catch(ex) {
@@ -14,8 +12,20 @@ async function pubmessage(){
     }
 }
 
-pubmessage()
-pubmessage()
+async function submessage(){
+    try {
+        let nc = await connect({ servers: ['nats://nats:4222', 'nats://nats-1:4222', 'nats://nats-2:4222']});
+    
+        let sub = await nc.subscribe('FrontQueue', (err, msg) => {
+            console.log('Message received on', msg.subject, ":", msg.data);
+            //WorkToDo
+        });
+    } catch(ex) {
+        console.log(ex)
+    }
+}
+
+submessage()
 pubmessage()
 pubmessage()
 pubmessage()

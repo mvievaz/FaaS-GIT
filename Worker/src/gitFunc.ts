0f0e4b37cb593acaba03d435b5d111c2eeba1fe2
mvievaz <quelviel@gmail.com>
2024-01-31 15:48:29 +0100

    feat: Worker work

--- Worker/src/gitFunc.ts
+++ Worker/src/gitFunc.ts
@@ -8,12 +8,10 @@ export function downloadGIT(gitURL: string): Promise<string> {
     return new Promise<string>((resolve, reject) => {
         git.clone(gitURL, localPATH).then(
             () => {
-                console.log("Repo downloaded")
                 resolve("Repo downloaded")
             }
         ).catch(
             (reason: any) => {
-                console.log("Error downloading git")
                 reject(`Error downloading repo ${localPATH}, ERROR: ${reason}`)
             }
         )
@@ -24,11 +22,9 @@ export function clearGIT(): Promise<string> {
     return new Promise<string>((resolve, reject) => {
         fs.rm(localPATH, { recursive: true, }, (error) => {
             if (error) {
-                console.log("Error clearing git");
                 reject(error)
             }
             else {
-                console.log("GIR cleared");
                 resolve("GIT cleared")
             }
         })
